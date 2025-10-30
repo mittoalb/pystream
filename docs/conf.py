13cf813fca63da9fd021b4fc56605eab63714c86
mittoalb <alberto.mittone@gmail.com>
2025-10-29 19:04:01 -0500

    logo

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
-html_logo = "./docs/_static/logo_gray.png"
+html_logo = "./_static/logo_gray.png"
 
 
 myst_enable_extensions = ["colon_fence", "deflist"]
